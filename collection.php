515c4bcce32dab33edb4fa9b5eac928066a711ba
Andreas Flack <flack@contentcontrol-berlin.de>
2012-05-31 11:16:53 +0200

    Add default doc blocks

--- collection.php
+++ collection.php
@@ -1,6 +1,18 @@
 <?php
+/**
+ * Collection holder
+ *
+ * @copyright CONTENT CONTROL GbR, http://www.contentcontrol-berlin.de
+ * @author CONTENT CONTROL GbR, http://www.contentcontrol-berlin.de
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @package openpsa.createphp
+ */
+
 namespace openpsa\createphp;
 
+/**
+ * @package openpsa.createphp
+ */
 class collection extends node implements \ArrayAccess, \Iterator
 {
     protected $_position = 0;
