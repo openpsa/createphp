056a274050306cf05712d389aab7ac242b9f7b0a
David Buchmann <david.buchmann@liip.ch>
2012-07-30 22:41:52 +0200

    adjust rest service and fix tests

--- tests/Entity/CollectionTest.php
+++ tests/Entity/CollectionTest.php
@@ -6,23 +6,47 @@ use Midgard\CreatePHP\Entity\Collection;
 
 class CollectionTest extends \PHPUnit_Framework_TestCase
 {
-    public function test_get_setController()
+    /**
+     * @var \Midgard\CreatePHP\Type\TypeInterface
+     */
+    private $parentController;
+    /**
+     * @var \Midgard\CreatePHP\Type\TypeInterface
+     */
+    private $childController;
+    private $mockTypeFactory;
+    /** @var \Midgard\CreatePHP\RdfMapperInterface */
+    private $mockMapper;
+
+    public function setUp()
     {
-        $controller = new Controller(new MockMapper);
-        $controller->setVocabulary('test', 'http:://test.org/');
-        $collection = new Collection('test', array());
-        $collection->setType($controller);
-        $this->assertEquals($controller, $collection->getType());
-        $this->assertEquals('http:://test.org/', $collection->getAttribute('xmlns:test'));
+        $this->mockMapper = new MockMapper;
+        $this->parentController = new Controller($this->mockMapper);
+        $this->parentController->setVocabulary('test', 'http:://test.org/');
+        $this->childController = new Controller($this->mockMapper);
+
+        $this->mockTypeFactory = $this
+            ->getMockBuilder('Midgard\\CreatePHP\\Metadata\\RdfTypeFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->mockTypeFactory->expects($this->any())
+            ->method('getType')
+            ->with('test')
+            ->will($this->returnValue($this->childController))
+        ;
+    }
+
+    public function test_get_setType()
+    {
+        $collection = new Collection('test', $this->mockTypeFactory, array());
+        $collection->setType('test');
+        $this->assertEquals('test', $collection->getType());
     }
 
     public function test_loadFromParent()
     {
-        $mapper = new MockMapper;
-        $parent_controller = new Controller($mapper);
-        $child_controller = new Controller($mapper, array('is_child' => true));
-        $collection = new Collection('test', array());
-        $collection->setType($child_controller);
+        $collection = new Collection('test', $this->mockTypeFactory, array('is_child' => true));
+        $collection->setType('test');
 
         $parent = array
         (
@@ -33,42 +57,36 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
                 array('id' => 'child2')
             )
         );
-        $parent_controller->test = $collection;
-        $parent_controller->setObject($parent);
-        $children = $parent_controller->test->getChildren();
+        $this->parentController->test = $collection;
+        $entity = $this->parentController->createWithObject($parent);
+        $children = $entity->test->getChildren();
         $this->assertEquals(2, sizeof($children));
         $this->assertInstanceOf('Midgard\CreatePHP\Entity\Controller', $children[0]);
-        $this->assertEquals('child1', $mapper->createSubject($children[0]->getObject()));
+        $this->assertEquals('child1', $this->mockMapper->createSubject($children[0]->getObject()));
     }
 
     public function test_loadFromParent_no_children()
     {
-        $mapper = new MockMapper;
-        $parent_controller = new Controller($mapper);
-        $child_controller = new Controller($mapper, array('is_child' => true));
-        $collection = new Collection('test', array());
-        $collection->setType($child_controller);
+        $collection = new Collection('test', $this->mockTypeFactory, array('is_child' => true));
+        $collection->setType('test');
 
         $parent = array
         (
             'id' => 'test_id'
         );
-        $parent_controller->test = $collection;
-        $parent_controller->setObject($parent);
-        $children = $parent_controller->test->getChildren();
+        $this->parentController->test = $collection;
+        $entity = $this->parentController->createWithObject($parent);
+        $children = $entity->test->getChildren();
         $this->assertEquals(1, sizeof($children));
         $this->assertInstanceOf('Midgard\CreatePHP\Entity\Controller', $children[0]);
-        $this->assertEquals('', $mapper->createSubject($children[0]->getObject()));
+        $this->assertEquals('', $this->mockMapper->createSubject($children[0]->getObject()));
        $this->assertEquals('display:none', $children[0]->getAttribute('style'));
     }
 
     public function test_Iterator()
     {
-        $mapper = new MockMapper;
-        $parent_controller = new Controller($mapper);
-        $child_controller = new Controller($mapper, array('is_child' => true));
-        $collection = new Collection('test', array());
-        $collection->setType($child_controller);
+        $collection = new Collection('test', $this->mockTypeFactory, array('is_child' => true));
+        $collection->setType('test');
 
         $parent = array
         (
@@ -79,10 +97,10 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
                 array('id' => 'child2')
             )
         );
-        $parent_controller->test = $collection;
-        $parent_controller->setObject($parent);
+        $this->parentController->test = $collection;
+        $entity = $this->parentController->createWithObject($parent);
         $i = 0;
-        foreach ($parent_controller->test as $key => $child)
+        foreach ($entity->test as $key => $child)
         {
             $this->assertInstanceOf('Midgard\CreatePHP\Entity\Controller', $child);
             $i++;
@@ -92,11 +110,8 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
 
     public function test_ArrayAccess()
     {
-        $mapper = new MockMapper;
-        $parent_controller = new Controller($mapper);
-        $child_controller = new Controller($mapper, array('is_child' => true));
-        $collection = new Collection('test', array());
-        $collection->setType($child_controller);
+        $collection = new Collection('test', $this->mockTypeFactory, array('is_child' => true));
+        $collection->setType('test');
 
         $parent = array
         (
@@ -107,13 +122,13 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
                 array('id' => 'child2')
             )
         );
-        $parent_controller->test = $collection;
-        $parent_controller->setObject($parent);
-        $this->assertInstanceOf('Midgard\CreatePHP\Entity\Controller', $parent_controller->test[0]);
-        unset($parent_controller->test[1]);
-        $this->assertFalse(isset($parent_controller->test[1]));
-        $parent_controller->test[1] = new Collection('test2', array());
-        $parent_controller->test[] = new Collection('test3', array());
-        $this->assertTrue(isset($parent_controller->test[2]));
+        $this->parentController->test = $collection;
+        $entity = $this->parentController->createWithObject($parent);
+        $this->assertInstanceOf('Midgard\CreatePHP\Entity\Controller', $entity->test[0]);
+        unset($entity->test[1]);
+        $this->assertFalse(isset($entity->test[1]));
+        $entity->test[1] = new Collection('test2', $this->mockTypeFactory, array());
+        $entity->test[] = new Collection('test3', $this->mockTypeFactory, array());
+        $this->assertTrue(isset($entity->test[2]));
     }
 }
